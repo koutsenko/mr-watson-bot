af9ac2b47dc7adbb02bc3e79978c0484a284e42e
Kutsenko Dmitry <koutsenko@gmail.com>
2021-07-22 02:25:01 +0300

    Implemented telegraf example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import { helloMessage } from './messages';
+import { config } from 'dotenv';
 
+import { helloMessage } from './messages';
+import { initBot } from './telegraf';
+
+config();
+initBot();
 console.log(helloMessage);
