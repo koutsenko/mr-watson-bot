22302452e6c3a51422bc7598e22b36c514fa42fe
Kutsenko Dmitry <koutsenko@gmail.com>
2021-08-30 21:05:52 +0300

    Workaround nodemon issue

--- src/util/log.ts
+++ src/util/log.ts
@@ -1,14 +1,25 @@
+import chalk from 'chalk';
+
 import { isVerboseMode } from '../checks/isVerbose';
 import { getReadableTimestamp } from './datetime';
+import { randomInt } from './number';
 
 /**
- * Печатает сообщение со штампом времени.
- *
- * @param message
+ * Печатает сообщение со цветным штампом времени. Цвет генерируется 1 раз.
  */
-export const log = (message: string): void => {
-  console.log(getReadableTimestamp(), message);
-};
+export const log = (() => {
+  const index = randomInt(0, 5).toString();
+  const color = {
+    0: chalk.red,
+    1: chalk.green,
+    2: chalk.yellow,
+    3: chalk.blue,
+    4: chalk.magenta,
+    5: chalk.cyan
+  }[index];
+
+  return (message) => console.log(color(getReadableTimestamp()), message);
+})();
 
 /**
  * Печатает сообщение со штампом времени, если включен verbose-режим.
