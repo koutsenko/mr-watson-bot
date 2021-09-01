d3945376f97acedbc87cab2158b9d3023330e7bc
Kutsenko Dmitry <koutsenko@gmail.com>
2021-09-02 01:46:44 +0300

    Add fake reaction to answer, go IDLE if no answer in 5 minutes

--- src/modules/bot/index.ts
+++ src/modules/bot/index.ts
@@ -10,6 +10,17 @@ interface IBotError extends Error {
   };
 }
 
+export interface IContext {
+  reply: (arg: string) => void;
+  update?: {
+    message?: {
+      from?: {
+        id;
+      };
+    };
+  };
+}
+
 /**
  * Обработчик ошибки инициализации бота.
  */
@@ -27,9 +38,9 @@ export const handleBotError = (error: IBotError): void => {
  * Декоратор для хэндлеров телеграфа, добавляет проверку на владельца.
  * Подразумевается, что первый аргумент хэндлера - это контекст бота.
  */
-const wrapOwner =
-  (originalMethod) =>
-  (...args) => {
+export const wrapOwner =
+  (originalMethod: (ctx: IContext) => void): ((ctx: IContext) => void) =>
+  (...args: IContext[]) => {
     const ctx = args[0];
     if (String(ctx?.update?.message?.from?.id) !== process.env.OWNER_CHAT_ID) {
       return;
@@ -38,20 +49,11 @@ const wrapOwner =
     return originalMethod.apply(this, args);
   };
 
-const handleHelp = wrapOwner((ctx) => ctx.reply(Messages.MSG_STICKER));
-const handleStart = wrapOwner((ctx) => ctx.reply(Messages.MSG_WELCOME));
-const handleSticker = wrapOwner((ctx) => ctx.reply(Messages.MSG_SUPER));
-const handleHi = wrapOwner((ctx) => ctx.reply(Messages.MSG_HELLO));
-
 /**
  * Инициализация модуля с Telegram Bot API.
  */
 export const initBot = async (): Promise<Telegraf> => {
   const bot = new Telegraf(process.env.BOT_TOKEN);
-  bot.start(handleStart);
-  bot.help(handleHelp);
-  bot.on('sticker', handleSticker);
-  bot.hears('hi', handleHi);
   await bot.launch();
 
   return bot;
--- src/scenarios/sleep/index.ts
+++ src/scenarios/sleep/index.ts
@@ -1,15 +1,50 @@
 /**
  * Сценарий: мониторинг сна
  * Пользователь: koutsenko
+ *
+ * Использует:
+ * - модуль планировщика задач, документация https://www.npmjs.com/package/node-schedule.
  */
+import * as schedule from 'node-schedule';
+
+import { everyMinute } from '../../constants/jobs';
+import { IContext, wrapOwner } from '../../modules/bot';
 import { IAppState } from '../../types/state';
-import { initSleepJob } from './jobs/sleep';
-import { localState } from './state';
+import { verbose } from '../../util/log';
+import {
+  handleIdleStateTick,
+  handleSleepAnswer,
+  handleWaitingForAnswerTick,
+  handleWaitingForSleepTick
+} from './actions';
+import { EScenarioState, localState } from './state';
 
 /**
- * Инициализация.
+ * Инициализация периодических задач и обработчиков сообщений пользователя .
  */
 export const init = (state: IAppState): void => {
   // Инициализация задачи на опрос состояния сна.
-  state.jobs.push(initSleepJob(state, localState));
+  const job: schedule = schedule.scheduleJob(everyMinute, async () => {
+    verbose(`sleep job cycle, current state: ${localState.state}`);
+    if (localState.state === EScenarioState.STATE_IDLE) {
+      await handleIdleStateTick(state);
+    } else if (localState.state === EScenarioState.STATE_WAITING_FOR_SLEEP) {
+      await handleWaitingForSleepTick(state);
+    } else if (localState.state === EScenarioState.STATE_WAITING_FOR_ANSWER) {
+      handleWaitingForAnswerTick();
+    }
+  });
+
+  // Сохранение ссылки на задачу.
+  state.jobs.push(job);
+
+  // Добавление реакции, пока эхо
+  state.bot_module.on(
+    'message',
+    wrapOwner((ctx: IContext) => {
+      if (localState.state === EScenarioState.STATE_WAITING_FOR_ANSWER) {
+        handleSleepAnswer(ctx);
+      }
+    })
+  );
 };
